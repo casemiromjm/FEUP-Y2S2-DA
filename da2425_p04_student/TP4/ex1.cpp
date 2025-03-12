dde042c504afee4a6523689bd7b0caf95d2b05ae
casemiromjm <casemiromelojorge@gmail.com>
2025-03-12 11:47:20 +0000

    done ex1tp4

--- da2425_p04_student/TP4/ex1.cpp
+++ da2425_p04_student/TP4/ex1.cpp
@@ -1,9 +1,78 @@
 #include "exercises.h"
 #include "cmath"
 
+int aux_recursion(int A[], int beg, int end, int &i, int &j) {
+    // base
+    if (beg == end) {
+        i = j = beg;
+        return A[beg];
+    }
+
+    // recursion
+    int beg2 = int(round((beg+end)/2.0));  // new starting point
+    int end1 = beg2-1;
+
+    int ir, jr;
+    int bestSumRight = aux_recursion(A, beg, end1, ir, jr);
+
+    int il, jl;
+    int bestSumLeft = aux_recursion(A, beg2, end, il, jl);
+
+    // check the other solution considering the middle
+    // left
+    auto curSum = A[end1];
+    auto bestCrossingSumLeft = A[end1];
+    auto iCrossing = end1;
+
+    for (int k = end1 -1; k >= beg; k--) {
+        curSum += A[k];
+
+        if (curSum > bestCrossingSumLeft) {
+            bestCrossingSumLeft = curSum;
+            iCrossing = k;
+        }
+    }
+
+    // right
+    curSum = A[beg2];
+    auto bestCrossingSumRight = A[beg2];
+    auto jCrossing = beg2;
+
+    for (int k = beg2 + 1; k <= end; k++) {
+        curSum += A[k];
+
+        if (curSum > bestCrossingSumRight) {
+            bestCrossingSumRight = curSum;
+            jCrossing = k;
+        }
+    }
+
+    auto bestCrossingSum = bestCrossingSumLeft + bestCrossingSumRight;
+
+    // compare all solutions
+    if ((bestCrossingSum > bestSumLeft) && (bestCrossingSum > bestSumRight)) {
+        i = iCrossing;
+        j = jCrossing;
+
+        return bestCrossingSum;
+    }
+    else if (bestSumLeft > bestSumRight) {
+        i = il;
+        j = jl;
+
+        return bestSumLeft;
+    }
+    else {
+        i = ir;
+        j = jr;
+
+        return bestSumRight;
+    }
+}
+
 int maxSubsequenceDC(int A[], unsigned int n, int &i, int &j) {
-    // TODO: Implement function
-    return 0;
+
+    return aux_recursion(A, 0, n-1, i, j);;
 }
 
 /// TESTS ///
